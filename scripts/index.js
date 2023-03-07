e84818338132cf1a7d9fb61a0449fec25ca2f298
Amir Zaitov <amir.zaitov.2019@mail.ru>
2023-03-07 16:59:20 +0300

    PR 5

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,36 @@ let popup = document.querySelector('.popup');
 let close = document.querySelector('.popup__close-button');
 let name = document.querySelector('.profile__name');
 let title = document.querySelector('.profile__title');
-
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__input_id_name');
 let jobInput = formElement.querySelector('.popup__input_id_title');
 
+// Профиль
+function handleOpen(){
+    nameInput.value = name.textContent
+    jobInput.value = title.textContent
+    popup.classList.add('popup_opened');
+};
 
+function handleClose(){
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', handleOpen);
+
+close.addEventListener('click', handleClose);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    name.textContent = nameInput.value;
+    title.textContent = jobInput.value;
+    handleClose();
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+
+// Добавить карточку
 
 function handleOpen(){
     nameInput.value = name.textContent
@@ -26,21 +48,93 @@ editButton.addEventListener('click', handleOpen);
 
 close.addEventListener('click', handleClose);
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault();
 
     name.textContent = nameInput.value;
     title.textContent = jobInput.value;
     handleClose();
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+const elements__list = document.querySelector(".elements__list"); 
+
+
+
+
+initialCards.forEach((card) => {
+    const element =  document.createElement("li")
+    element.classList.add("element")
+
+    let flag = false
+
+
+    const button = document.createElement("button")
+    button.classList.add("element__delete-button")
+    button.addEventListener("click", () => {
+        deleteCard(element)
+    })
+
+    const image = document.createElement("img")
+    image.classList.add("element__mask-group")
+    image.src = card.link
+
+    const element__container = document.createElement("div")
+    element__container.classList.add("element__container")    
+    const name = document.createElement("h2")
+    name.classList.add("element__text")
+    name.textContent = card.name
+
+    const button2 = document.createElement("button")
+    button2.classList.add("element__like-button")
+    
+    button2.addEventListener("click", () => {
+        if(!flag){ 
+            button2.classList.add("black__heart")
+            flag=true
+        }
+        else{
+            button2.classList.remove("black__heart")
+            flag=false
+        }
+    })
+    
+    elements__list.appendChild(element)
+    element.appendChild(image)
+    element.appendChild(button)
+    element.appendChild(element__container)     
+    element__container.appendChild(name)
+    element__container.appendChild(button2)
+})
+
+
+
+
+
